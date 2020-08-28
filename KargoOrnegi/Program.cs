18f3e5c7a09beda32b9f70fd716371565f67a98e
kurulerdem <kurulerdem1@gmail.com>
2020-08-28 21:51:41 +0300

    Soyutlama Kargo Ornegi

---
+++ KargoOrnegi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KargoOrnegi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KargoYonet kargoYonet = new KargoYonet();
+            kargoYonet.gonder = new SubeGonder();
+            kargoYonet.Add();
+            Console.ReadLine();
+        }
+    }
+
+    class KargoYonet
+    {
+        public IGonder gonder { get; set; }
+        public void Add()
+        {
+            gonder.Gonder();
+        }
+    }
+
+
+
+    interface IGonder
+    {
+        void Gonder();
+    }
+
+    class AdresGonder : IGonder
+    {
+    public void Gonder()
+        {
+            Console.WriteLine("Adrese Gonderildi");
+        }
+    }
+
+    class SubeGonder : IGonder
+    {
+        public void Gonder()
+        {
+            Console.WriteLine("Subeye Gonderildi");
+        }
+
+       
+    }
+}
